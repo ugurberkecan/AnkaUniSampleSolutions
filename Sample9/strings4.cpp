f29375c5870970eb8f75a682420f653e4729d800
ugur unlu <ugurberkecanunlu@gmail.com>
2019-05-01 14:09:46 +0300

    better now

--- Sample9/strings4.cpp
+++ Sample9/strings4.cpp
@@ -14,19 +14,23 @@ int main(){
   string s1;
   string s2;
   int counter=0;
+  int counter2=0;
   int i=0;
   int k=0;
   cin >> s1;
   cin >> s2;
   while( s1[counter] != '\0'){
   counter++;
-}s1[counter++]=' ';
+}
+  while( s2[counter2] != '\0'){
+counter2++;
+}
+  s1.resize(counter+counter2);
   while(s2[i] != '\0'){
       s1[counter++]=s2[i++];
 
   }s1[counter]='\0';
-  while(s1[k] != '\0'){
-    cout << s1[k++];
-}
+    cout << s1; //  output is computer
+
 
 }
