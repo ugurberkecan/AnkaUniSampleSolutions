6f9054125e9521fc8c1d653a0d89ffaa971642aa
ugur unlu <ugurberkecanunlu@gmail.com>
2019-05-01 20:21:29 +0300

    last edit

--- Sample9/strings7.cpp
+++ Sample9/strings7.cpp
@@ -17,39 +17,23 @@
     return 0;
   }
   int foundstring(string string1,string string2){
-  int counter=0;
-  int i=0;
+  int counter=0; //
   int counter2=0;
+  int i=0;
   int value=0;
-  while(string1[counter]!= '\0'){
-    counter++;
-  }
-  counter--;
-  while(string2[counter2]!= '\0'){
-    counter2++;
-  }
-  counter2--;
-  for(int k=0;k<counter2+1;k++){
+  while(string1[++counter]!= '\0'){}
+  while(string2[++counter2]!= '\0'){}
   for(i;i<counter+1;i++){
-    value=0;
-    if(string1[i] == string2[k]) {                // computer ,put
-      int m=i+1;
-      value++;
-      for(int p=k+1;p<counter2+1;p++){
-
-        if(string1[m++] == string2[p]){
-          value++;
-        }
-          else {
-            break;
+    if(string1[i] == string2[0])
+      {
+      value=0;
+    for(int k=0;k<counter2;k++)
+          {
+      if( string1[i+k] == string2[k])
+        value++;
+        if(counter2 == value)
+            return i+1;
           }
-          if(value == counter2+1)
-              return i+1;
-        }
-
       }
-
-    }
-
-    }
   }
+}
